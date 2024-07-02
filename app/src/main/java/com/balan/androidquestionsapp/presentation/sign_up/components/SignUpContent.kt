07d0fbeb88b763be84af0c06581808c60f76c88a
boonapart <kolyha101@gmail.com>
2024-07-02 16:29:58 +0300

    user interface

--- app/src/main/java/com/balan/androidquestionsapp/presentation/sign_up/components/SignUpContent.kt
+++ app/src/main/java/com/balan/androidquestionsapp/presentation/sign_up/components/SignUpContent.kt
@@ -9,32 +9,55 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.balan.androidquestionsapp.R
 import com.balan.androidquestionsapp.presentation.topbar.TopBar
 import com.balan.androidquestionsapp.ui.theme.Background
-import com.balan.androidquestionsapp.ui.theme.LocalDimen
-import com.balan.androidquestionsapp.ui.theme.LocalProperty
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun SignUpScreenPreview() {
+    SignUpContent(
+        state = SignUpState(),
+        onLoginChange = {},
+        onPasswordChange = {},
+        onEmailChange = {},
+        onSignUpClick = {},
+        onSignInClick = {},
+        isFieldsNotEmpty = { false },
+        modifier = Modifier,
+    )
+}
 
 @Composable
 fun SignUpContent(
@@ -57,122 +80,97 @@ fun SignUpContent(
     ) {
         Box(modifier = Modifier.padding(it))
         Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
             modifier = modifier
                 .fillMaxSize()
-                .background(Background),
-            verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .background(Background)
+                .padding(16.dp)
         ) {
-
-            Box(
-                modifier = Modifier.weight(LocalProperty.current.weight1),
-                contentAlignment = Alignment.Center
+            Icon(
+                imageVector = Icons.Filled.Face,
+                contentDescription = null,
+                modifier = Modifier.size(130.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = stringResource(id = R.string.registration),
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            TextFieldBox(
+                value = state.email,
+                text = stringResource(id = R.string.input_email),
+                imageVector = Icons.Filled.Email,
+                onValueChange = onEmailChange,
+                imeAction = ImeAction.Next
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TextFieldBox(
+                value = state.name,
+                text = stringResource(id = R.string.input_login),
+                imageVector = Icons.Filled.AccountBox,
+                onValueChange = onLoginChange,
+                imeAction = ImeAction.Next
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TextFieldBox(
+                value = state.password,
+                text = stringResource(id = R.string.input_password),
+                imageVector = ImageVector.vectorResource(R.drawable.baseline_password_24),
+                onValueChange = onPasswordChange,
+                imeAction = ImeAction.Done
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(
+                onClick = onSignUpClick,
+                colors = ButtonDefaults.buttonColors(Color.Black),
+                modifier = Modifier.fillMaxWidth(),
+                enabled = isFieldsNotEmpty(),
+                shape = RoundedCornerShape(10.dp)
             ) {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Spacer(modifier = Modifier.padding(top = LocalDimen.current.spacerPaddingTop64))
-
-                    SignUpTextField(
-                        value = state.email,
-                        label = R.string.input_email,
-                        onValueChange = onEmailChange,
-                        imeAction = ImeAction.Next
-                    )
-
-                    Spacer(modifier = Modifier.padding(top = LocalDimen.current.spacerPaddingTop32))
-
-                    SignUpTextField(
-                        value = state.password,
-                        label = R.string.input_password,
-                        onValueChange = onPasswordChange,
-                        imeAction = ImeAction.Next
-                    )
-
-                    Spacer(modifier = Modifier.padding(top = LocalDimen.current.spacerPaddingTop32))
-
-                    SignUpTextField(
-                        value = state.name,
-                        label = R.string.input_login,
-                        onValueChange = onLoginChange,
-                        imeAction = ImeAction.Done
-                    )
-
-                    Spacer(modifier = Modifier.padding(top = LocalDimen.current.spacerPaddingTop32))
-                    Button(
-                        onClick = onSignUpClick,
-                        modifier = Modifier.width(LocalDimen.current.buttonSignUpWidth),
-                        colors = ButtonDefaults.buttonColors(Color.White),
-                        enabled = isFieldsNotEmpty()
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.sign_up), color = Color.Black,
-                            fontSize = LocalDimen.current.buttonTextSize24,
-                        )
-                    }
-                    Spacer(modifier = Modifier.padding(top = LocalDimen.current.spacerPaddingTop32))
-
-                    Text(text = stringResource(id = state.valid.textResId))
-                }
+                Text(
+                    text = stringResource(id = R.string.register),
+                    fontSize = 16.sp
+                )
             }
+            Text(
+                text = stringResource(id = state.valid.textResId),
+                color = Color.Black,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
-
 }
 
-
 @Composable
-fun SignUpTextField(
+fun TextFieldBox(
     value: String,
-    label: Int,
+    imeAction: ImeAction,
+    text: String,
     onValueChange: (String) -> Unit,
-    imeAction: ImeAction
+    imageVector: ImageVector
 ) {
     OutlinedTextField(
         value = value,
         onValueChange = onValueChange,
-        textStyle = TextStyle(
-            textAlign = TextAlign.Center,
-            fontSize = LocalDimen.current.outlinedTextSize
-        ),
-        label = {
-            Text(
-                text = stringResource(id = label),
-                modifier = Modifier.fillMaxWidth(),
-                fontSize = LocalDimen.current.textSize24,
-                textAlign = TextAlign.Center
+        label = { Text(text = text) },
+        leadingIcon = {
+            Icon(
+                imageVector = imageVector,
+                contentDescription = null,
+                modifier = Modifier.size(30.dp)
             )
         },
-        shape = RoundedCornerShape(LocalDimen.current.outlinedShape),
-        modifier = Modifier
-            .clip(RoundedCornerShape(LocalDimen.current.outlinedClip))
-            .width(LocalDimen.current.outlinedTextWidth)
-            .height(LocalDimen.current.outlinedTextHeight)
-            .background(Color.White),
+        singleLine = true,
         keyboardOptions = KeyboardOptions(
             keyboardType = KeyboardType.Text,
             imeAction = imeAction
-        )
-    )
-}
-
-
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-@Composable
-fun SignUpScreenPreview() {
-    SignUpContent(
-        state = SignUpState(),
-        onLoginChange = {},
-        onPasswordChange = {},
-        onEmailChange = {},
-        onSignUpClick = {},
-        onSignInClick = {},
-        isFieldsNotEmpty = { false },
-        modifier = Modifier,
+        ),
+        modifier = Modifier.fillMaxWidth()
     )
 }
